c0b128f050cfc86cf8e460a421514158f3e536af
krimpas <61614176+krimpas@users.noreply.github.com>
2024-03-06 19:27:40 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.11.8-slim
+
+WORKDIR /drone/src
+
+ENV PROJECT_ROOT_DIR=/drone/src
+ENV DATA_DIR=${PROJECT_ROOT_DIR}/data
+ENV YML_DIR=${DATA_DIR}/yml
+ENV INV_DIR=${DATA_DIR}/inv
+ENV SCHEMA_DIR=${DATA_DIR}/schema/include.d
+ENV VALID_DIR=${DATA_DIR}/schema
+ENV J2_DIR=${DATA_DIR}/j2
+ENV CONFIG_DIR=${DATA_DIR}/snapshots/configs
+ENV NORNIR_CONFIG_DIR=${PROJECT_ROOT_DIR}/inv
+ENV NORNIR_CONFIG_FILE=${PROJECT_ROOT_DIR}/config.yml
+ENV CODE_DIR=${PROJECT_ROOT_DIR}/code
+
+# Install dependencies:
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+RUN pip install yamllint
---
+++ tasks.py
@@ -0,0 +1,24 @@
+from invoke import task
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@task
+def lint_yaml(c, directory=f"{os.environ.get('YML_DIR')}/r*/", file_pattern="*.yml"):
+    """
+    Run yamllint on YAML files in a directory using wildcards.
+    """
+    c.run(f"yamllint {directory}/{file_pattern}")
+
+@task
+def black(c, directory="."):
+    """
+    Run yamllint on YAML files in a directory using wildcards.
+    """
+    c.run(f"black {directory} --check --color")
+
+@task
+def check(c, section='interfaces'):
+    c.run(f"python3 code/checkconfig.py --section {section}")
