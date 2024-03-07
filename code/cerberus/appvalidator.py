672c06262dd5a454ed24f9ef88fdf51d324d1515
krimpas <krimpas@yahoo.gr>
2024-03-07 17:43:19 +0200

    another commit 3

---
+++ code/cerberus/appvalidator.py
@@ -0,0 +1,184 @@
+from cerberus import Validator
+from ipaddress import ip_address, ip_interface
+from dotenv import load_dotenv
+import yaml
+import os
+
+# Loads the environment.
+load_dotenv()
+
+
+def load_valid_data(schdir: str = "VALID_DIR"):
+    """Loads custom valid data YAML"""
+    valid_dir = os.environ.get("VALID_DIR")
+    valid_file = f"{valid_dir}/validata.yml"
+    with open(valid_file, "r") as f:
+        valid_data = yaml.safe_load(f)
+    return valid_data
+
+
+class L3ifacevalidator(Validator):
+    """
+    Cerberus-like Class that performs validation on L3 interfaces,
+
+    Methods
+    -------
+    _validate_type_ipv4addr
+        Validates if a yaml element is actually an ipv4address
+
+    _validate_type_netmask
+        Validates if a yaml element is actually a netmask
+
+    _validate_type_ospf_net_type
+        Validates if a yaml element is actualy an enumeration
+        of OSPF network type.
+
+    """
+
+    def _validate_type_ipv4addr(self, value):
+        """
+        Performs a validation if value is a valid ipv4addr.
+
+        Parameters
+        ----------
+        value : str
+            It will be checked if ia a valid ipv4addr.
+
+        Returns
+        -------
+        bool:
+            True if a valid ipv4addr, otherwise False.
+        """
+
+        try:
+            if ip_address(value):
+                return True
+        except ValueError:
+            return False
+
+    def _validate_type_netmask(self, value):
+        """
+        Performs a validation if value is a valid netmask.
+
+        Parameters
+        ----------
+        value : str
+            The value that will be checked if ia a valid netmask.
+
+        Returns
+        -------
+        bool:
+            True if a valid netmask, otherwise False.
+        """
+
+        try:
+            sm = value + "/" + value
+            addr = ip_interface(sm)
+            if addr.network.network_address:
+                return True
+        except ValueError:
+            return False
+
+    def _validate_type_privipv4addr(self, value):
+        """
+        Performs a validation if value is a valid Private ipv4addr.
+        10.0.0.0/8 172.16.0.0/12 192/168.0.0/16
+
+        Parameters
+        ----------
+        value : str
+            It will be checked if ia a valid Private ipv4addr.
+
+        Returns
+        -------
+        bool:
+            True if a valid Private ipv4addr, otherwise False.
+        """
+
+        try:
+            if ip_address(value).is_private:
+                return True
+        except ValueError:
+            return False
+
+    def _validate_type_ospf_net_type(self, value):
+        """
+        Validates if a yaml element is actualy an enumeration
+        of OSPF network type.
+
+        Parame
+        ters
+        ----------
+        value : str
+            It will be checked if ia a valid OSPF network type.
+
+        Returns
+        -------
+        bool:
+            True if a valid OSPF network type, otherwise False.
+        """
+
+        valid = load_valid_data()
+        return True if value.lower() in valid["ospf_nets"] else False
+
+
+class Ospfvalidator(Validator):
+    """
+    Class For OSPF schema validation using Cerberus-like methods
+
+    Methods
+    -------
+    _validate_type_ipv4addr
+        Validates if a value is a valid ipv4addr.
+
+    """
+
+    def _validate_type_ipv4addr(self, value):
+        """
+        Performs a validation if value is a valid ipv4addr.
+
+        Parameters
+        ----------
+        value : str
+            It will be checked if ia a valid ipv4addr.
+
+        Returns
+        -------
+        bool:
+            True if a valid ipv4addr, otherwise False.
+        """
+        try:
+            if ip_address(value):
+                return True
+        except ValueError:
+            return False
+
+
+class Keychainsvalidator(Validator):
+    """
+    Class For Keychains schema validation.
+
+    Methods
+    -------
+    _validate_type_crypto_algorithm
+        Validates if a value is a valid crypto algorithm.
+
+    """
+
+    def _validate_type_crypto_algorithm(self, value):
+        """
+        Performs a validation if value is a valid crypto algorithm.
+
+        Parameters
+        ----------
+        value : str
+            It will be checked if ia a valid crypto algorithm.
+
+        Returns
+        -------
+        bool:
+            True if a valid crypto algorithm, otherwise False.
+        """
+
+        valid = load_valid_data()
+        return True if value.lower() in valid["crypto_algorithms"] else False
---
+++ code/cerberus/checkconfig.py
@@ -0,0 +1,62 @@
+import os
+import logging
+import argparse
+from dotenv import load_dotenv
+from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_utils.plugins.functions import print_result, print_title
+from nornir.core.filter import F
+from sectionvalidator import SectionValidator
+from pprint import pprint
+
+load_dotenv()
+
+
+def main():
+
+    # Define directories and section
+    data_directory = os.environ.get("YML_DIR")
+    schema_directory = os.environ.get("SCHEMA_DIR")
+
+    # Prepare the argparse command line arguments.
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s",
+        "--section",
+        type=str,
+        required=False,
+        default="all",
+        help=f"Defines the configuration section",
+    )
+
+    sargs = parser.parse_args()
+
+    # Initialize Nornir
+    nr = InitNornir(config_file=os.environ.get("NORNIR_CONFIG_FILE"))
+
+    # Filet inventory using section
+    filtered_hosts = nr.filter(F(conf_sections__contains=sargs.section))
+
+    # Initialize SectionValidator
+    val = SectionValidator(data_directory, schema_directory, sargs.section)
+
+    # Run the validation task on all filtered hosts
+    result = filtered_hosts.run(
+        name=f"YAML Valitation Config: {sargs.section}",
+        task=val.validate_data_against_schema,
+        severity_level=logging.INFO,
+    )
+
+    # Print the results
+    print_result(result)
+
+    for h in filtered_hosts.inventory.hosts.keys():
+        if not result[h].result["is_valid"]:
+            exit(-123)
+    print_title(f">>>|{sargs.section} YAML check: PASSED |<<<")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
