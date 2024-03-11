dc4243aa7514d48fe762260a45b8e514a7825a36
krimpas <krimpas@yahoo.gr>
2024-03-11 23:36:55 +0200

    modified batfish directory

--- code/batfish/L3check.py
+++ code/batfish/L3check.py
@@ -1,16 +1,17 @@
 from L3_info import L3InterfaceInfo
 from bfish_L3iface_props import BFISH_L3IFACE_PROPS, L3IFACE_TYPES
 
+
 def main():
-    
+
     iface = L3InterfaceInfo(properties=BFISH_L3IFACE_PROPS.select_properties())
     print("\n")
-    #variable=f"Checking Gigabit Ethernet subnets"
-    #print(f"|< {variable:-^78} >|")
-    print(iface.check_L3_interface(anet="10.0.0.0/8", ifacetype="Gig")) 
+    # variable=f"Checking Gigabit Ethernet subnets"
+    # print(f"|< {variable:-^78} >|")
+    print(iface.check_L3_interface(anet="10.0.0.0/8", ifacetype="Gig"))
     print("\n")
-    #variable2= f"Checking Loopback subnets"
-    #print(f"|< {variable2:-^78} >|")
+    # variable2= f"Checking Loopback subnets"
+    # print(f"|< {variable2:-^78} >|")
     print(iface.check_L3_interface(anet="172.16.0.0/16", ifacetype="Loop"))
     print("\n")
 
