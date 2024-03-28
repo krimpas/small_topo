679f0ea951730b4aa9b34578f2a1c73f1de3caf1
krimpas <krimpas@yahoo.gr>
2024-03-28 18:28:19 +0200

    Adding Batfish Offline Checks

--- code/batfish/L3check.py
+++ code/batfish/L3check.py
@@ -5,13 +5,13 @@ from bfish_L3iface_props import BFISH_L3IFACE_PROPS
 def main():
 
     iface = L3InterfaceInfo(
-        node="r1", properties=BFISH_L3IFACE_PROPS.select_properties()
+        node="r5", properties=BFISH_L3IFACE_PROPS.select_properties()
     )
     print("\n")
 
     # variable=f"Checking Gigabit Ethernet subnets"
     # print(f"|< {variable:-^78} >|")
-    print(iface.check_L3_interface(anet="10.0.0.0/29", ifacetype="Gig"))
+    print(iface.check_L3_interface(anet="10.0.0.0/27", ifacetype="Gig"))
     print("\n")
     # variable2= f"Checking Loopback subnets"
     # print(f"|< {variable2:-^78} >|")
