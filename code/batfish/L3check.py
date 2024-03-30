a600e95928912b330263675822209ec47ea83ebc
krimpas <krimpas@yahoo.gr>
2024-03-31 00:46:35 +0200

    adding Nornir for Batfish Checksv2

--- code/batfish/L3check.py
+++ code/batfish/L3check.py
@@ -30,8 +30,8 @@ def main():
     result = nr.run(
         name="L3 GigaBit Batfish checks",
         task=exec_task,
-        anet="10.0.0.0/8",
-        ifacetype="Gig",
+        anet="172.16.0.0/16",
+        ifacetype="Loop",
         severity_level=logging.INFO,
     )
 
