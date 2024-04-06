7c59188cb1a2b3c1f9edf9382a4795098beee37f
krimpas <krimpas@yahoo.gr>
2024-04-06 21:59:33 +0300

    Checking for dups v8

--- code/batfish/L3check.py
+++ code/batfish/L3check.py
@@ -44,8 +44,8 @@ def main():
 
     if result:
         dfprint(
-            df=result.result.ifacelist,
-            props=["#"] + [c for c in result.result.ifacelist.columns],
+            df=result.ifacelist,
+            props=["#"] + [c for c in result.ifacelist.columns],
             title="Checking for L3 interfaces!",
         )
 
