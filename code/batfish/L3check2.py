8b9885d0f7d35820ce4e0b76780532ddb95ab5aa
krimpas <krimpas@yahoo.gr>
2024-04-07 03:43:17 +0300

    Checking for dups v9.9

--- code/batfish/L3check2.py
+++ code/batfish/L3check2.py
@@ -38,6 +38,7 @@ def exec_task2(task: Task, bf: Session, anet: str = "", ifacetype: str = "") -> 
         props=["#"] + [c for c in dups.columns],
         title="Checking for duplicates IPv4 address in the topology!",
     )
+
     topo = device.L3topo
     data = dfprint(
         df=topo,
