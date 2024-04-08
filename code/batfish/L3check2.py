dc2f01e537e777da46edc041f6f6ec76e4152593
krimpas <krimpas@yahoo.gr>
2024-04-08 20:15:29 +0300

    Checking for dups v9.137

--- code/batfish/L3check2.py
+++ code/batfish/L3check2.py
@@ -46,7 +46,7 @@ def exec_task2(task: Task, bf: Session, anet: str = "", ifacetype: str = "") -> 
         title="Checking for L3 topology!",
     )
 
-    return Result(host=task.host, result=dict(dups=dups, data=data))
+    return Result(host=task.host, result=dict(dups=dups))
 
 
 def main():
