d1afa93aef71fce40a7a430c37883626236fa560
krimpas <krimpas@yahoo.gr>
2024-05-08 16:13:51 +0300

    New functionality batfish source code

--- code/batfish/l3info.py
+++ code/batfish/l3info.py
@@ -174,12 +174,13 @@ class NodeL3InterfaceInfo:
             lambda x: set(reference_set) - set(x)
         )
 
-        diff_df = pd.DataFrame(
-            {
-                pd.Series(self.all_configured["Interfaces"]),
-                pd.Series(unexpected_interfaces),
-                pd.Series(missing_set),
-            },
+        diff_df = pd.concat(
+            [
+                pd.DataFrame(self.all_configured["Interfaces"]),
+                pd.DataFrame(unexpected_interfaces),
+                pd.DataFrame(missing_set),
+            ],
+            axis=0,
         )
 
         indicator = "NA"
