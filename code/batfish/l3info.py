e74f9c36d669f3700a9f5c3818c05d37aba5f6db
krimpas <krimpas@yahoo.gr>
2024-05-11 16:28:56 +0300

    New functionality batfish source code

--- code/batfish/l3info.py
+++ code/batfish/l3info.py
@@ -167,7 +167,7 @@ class NodeL3InterfaceInfo:
         reference_set = ifaces[self.node]
 
         unexpected_interfaces = self.all_configured["Interfaces"].map(
-            lambda x: set(x) - set(reference_set)
+            lambda x: list(x) - (reference_set)
         )
 
         diff_df = pd.DataFrame(unexpected_interfaces)
@@ -185,7 +185,7 @@ class NodeL3InterfaceInfo:
         reference_set = ifaces[self.node]
 
         missing_interfaces = self.all_configured["Interfaces"].map(
-            lambda x: set(reference_set) - set(x)
+            lambda x: reference_set - list(x)
         )
 
         diff_df = pd.DataFrame(missing_interfaces)
