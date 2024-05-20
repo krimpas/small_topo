0bcdccc980e13c52ba52549bf2cf56f85d528ec7
krimpas <krimpas@yahoo.gr>
2024-05-20 19:44:48 +0300

    Code Major revision

--- code/batfish/toponodel3.py
+++ code/batfish/toponodel3.py
@@ -110,7 +110,7 @@ class L3TopoNode:
         sot_interfaces = pd.DataFrame({"Interfaces": nodedict[self.node]})
         # Calculate the Unexpected configured interfaces
         unexpected_ifaces = actual_interfaces[
-            ~actual_interfaces.Interfaces.isin(sot_interfaces.Interfaces)
+            ~actual_interfaces.Interfaces.isin(nodedict[self.node])
         ]
 
         self.layer3_unexpected = unexpected_ifaces
