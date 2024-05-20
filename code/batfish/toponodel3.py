38d39bd511accc317f6258be8ffca335b0d0a7fe
krimpas <krimpas@yahoo.gr>
2024-05-20 19:23:18 +0300

    Code Major revision

--- code/batfish/toponodel3.py
+++ code/batfish/toponodel3.py
@@ -103,7 +103,7 @@ class L3TopoNode:
             configuration
         """
         # fetch the list of configured interfaces
-        configured = self.layer3_configured["Interfaces"].to_list()
+        configured = self.layer3_configured.iloc[0]["Interfaces"]
         # create a list of interfaces based on actual configuration
         actual_interfaces = pd.DataFrame({"Interfaces": configured})
         # create a dataframe of interfaces based on SoT
