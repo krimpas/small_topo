9fbf0e2855e2fcb484570db73fd319df92ae1bbd
krimpas <krimpas@yahoo.gr>
2024-05-21 17:35:07 +0300

    Code revision for nodel3

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -38,7 +38,9 @@ def exec_checks(
     )
 
     res = device.call_method_by_name(func_name, **kwargs)
-    data = dfprint(df=res, props=["#"] + list(res.columns), title=title)
+    data = dfprint(
+        df=res, props=["#"] + list(res.columns), title=title + f"Host={task.host.name}"
+    )
     # print(res)
     return Result(host=task.host, result=data)
 
