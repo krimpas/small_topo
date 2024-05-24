8af0975ab23dd6468443168b26e3359eb27cb8a2
krimpas <krimpas@yahoo.gr>
2024-05-25 01:16:05 +0300

    Class revision 1.3

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -76,7 +76,7 @@ def main():
     tmp_df = pd.concat(tmp_list_df, axis=0)
     print(tmp_df.reset_index(drop=True))
 
-    print_result(result)
+    print_result(result.data)
 
 
 if __name__ == "__main__":
