d715257de7c1c0b53da265fca8da5dc2986fa1df
krimpas <krimpas@yahoo.gr>
2024-05-25 00:57:13 +0300

    Class revision 1.3

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -70,7 +70,7 @@ def main():
     for h in nr.inventory.hosts.keys():
         print(result[h].data)
         print(result[h].statistics)
-        print_result(result[h])
+        print_result(result[h].data)
         print("---------------------------------------")
 
 
