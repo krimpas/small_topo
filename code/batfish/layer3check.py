755ad2a1d6e4ed64d2bc17b089a58dd72996bf5f
krimpas <krimpas@yahoo.gr>
2024-05-25 01:41:23 +0300

    Class revision 1.3

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -74,15 +74,15 @@ def main():
         tmp_list_df.append(result[h].statistics)
 
     tmp_df = pd.concat(tmp_list_df, axis=0)
-    print(tmp_df.reset_index(drop=True))
+    tmp = tmp_df.reset_index(drop=True)
 
     # for h in nr.inventory.hosts.keys():
     #    print_result(result[h], vars=["data", "statistics"])
 
     tmp_df_data = dfprint(
-        df=tmp_df,
-        props=["#"] + list(tmp_df.columns),
-        title=f"Statistics",
+        df=tmp,
+        props=["#"] + list(tmp.columns),
+        title="Statistics",
     )
     print(tmp_df_data)
 
