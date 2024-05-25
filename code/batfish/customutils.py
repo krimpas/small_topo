dd919c3b28ea0677587c3182bcada1ef86181ae5
krimpas <krimpas@yahoo.gr>
2024-05-25 18:29:49 +0300

    Class revision 1.G13

---
+++ code/batfish/customutils.py
@@ -0,0 +1,38 @@
+from prettytable import PrettyTable
+import pandas as pd
+
+
+def process_stats(result):
+    """Stas processing"""
+
+    for h, res in result.items():
+        # print(f"TYPE OF RES -> {type(res)}")
+        print(f"TYPE OF RES -> {type(res.result)}")
+
+        res.result.insert(0, "Device", [f"{h}"], True)
+
+    tmp_list_df = []
+    for h, res in result.items():
+        tmp_list_df.append(res.result)
+
+    tmp_df = pd.concat(tmp_list_df, axis=0)
+    tmp = tmp_df.reset_index(drop=True)
+
+    return tmp
+
+
+def dataframe_to_prettytable(
+    df: pd.DataFrame, title: str = "Error Elements"
+) -> PrettyTable:
+    """Creates a Pretty Table from a Dataframe"""
+    # Create PrettyTable object
+    table = PrettyTable()
+
+    if df is not None:
+        # Add columns
+        table.field_names = ["#"] + df.columns.tolist()
+        # Add rows
+        for row in df.itertuples():
+            table.add_row(row)
+
+    return table.get_string(title=title)
