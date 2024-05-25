3f733cb759af3aef129ea167be9de72dbc9b7dc0
krimpas <krimpas@yahoo.gr>
2024-05-25 12:51:13 +0300

    Class revision 1.13

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -67,7 +67,7 @@ def main():
         title="Erroneous L3 Interface Configuration",
         severity_level=logging.DEBUG,
     )
-    print_result(result["data"])
+    print_result(result, vars=["data"])
 
     tmp_list_df = []
     for h in nr.inventory.hosts.keys():
