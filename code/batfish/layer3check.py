022a18ab19f5e5ccd37718308ef73b3fa0d22077
krimpas <krimpas@yahoo.gr>
2024-05-25 14:43:43 +0300

    Class revision 1.d13

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -65,7 +65,7 @@ def dataframe_to_prettytable(df: pd.DataFrame) -> PrettyTable:
     table.field_names = df.columns.tolist()
 
     # Add rows
-    for row in df.itertuples(index=True):
+    for row in df.itertuples():
         table.add_row(row)
 
     return table
