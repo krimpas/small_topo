9b4502cc94807db82bea3232240c2c201f31c7bb
krimpas <krimpas@yahoo.gr>
2024-05-25 18:12:22 +0300

    Class revision 1.G13

--- code/batfish/toponodel3.py
+++ code/batfish/toponodel3.py
@@ -250,7 +250,7 @@ class TopoNodeL3Interface:
 
         stats = {
             "retcode": [error_code],
-            "SoT": [self.layer3_sot],
+            "SoT": [self.layer3_sot.shape[0]],
             "Unexpected": [self.layer3_unexpected.shape[0]],
             "Missing": [self.layer3_missing.shape[0]],
             "Status": [status],
