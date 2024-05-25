99b243c93404b94e27e3e0b1ae87e73d98a125ff
krimpas <krimpas@yahoo.gr>
2024-05-25 17:32:04 +0300

    Class revision 1.d13

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -114,7 +114,7 @@ def main():
     # tmp = process_stats(nr, statistics_result)
     # print(tmp)
     for host, task_result in statistics_result.items():
-        print(task_result)
+        print(task_result[host])
 
 
 if __name__ == "__main__":
