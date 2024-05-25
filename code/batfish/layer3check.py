ea37138a312c40b4e3b30498d2c376e9063d146c
krimpas <krimpas@yahoo.gr>
2024-05-25 18:29:19 +0300

    Class revision 1.G13

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 from bfish_init import bfish_init
 from pybatfish.client.session import Session
 from toponodel3 import NodeSection, TopoNodeL3Interface
-from prettytable import PrettyTable
+from customutils import process_stats, dataframe_to_prettytable
 
 ifaces = {
     "r1": ["GigabitEthernet2", "GigabitEthernet4", "Loopback0"],
@@ -38,42 +38,6 @@ def exec_checks(task: Task, bf: Session, func_name: str = "", **kwargs) -> Resul
     result_data = dfs.call_method_by_name(func_name, **kwargs)
 
     return Result(host=task.host, result=result_data)
-
-
-def process_stats(result):
-    """Stas processing"""
-
-    for h, res in result.items():
-        # print(f"TYPE OF RES -> {type(res)}")
-        print(f"TYPE OF RES -> {type(res.result)}")
-
-        res.result.insert(0, "Device", [f"{h}"], True)
-
-    tmp_list_df = []
-    for h, res in result.items():
-        tmp_list_df.append(res.result)
-
-    tmp_df = pd.concat(tmp_list_df, axis=0)
-    tmp = tmp_df.reset_index(drop=True)
-
-    return tmp
-
-
-def dataframe_to_prettytable(
-    df: pd.DataFrame, title: str = "Error Elements"
-) -> PrettyTable:
-    """Creates a Pretty Table from a Dataframe"""
-    # Create PrettyTable object
-    table = PrettyTable()
-
-    if df is not None:
-        # Add columns
-        table.field_names = ["#"] + df.columns.tolist()
-        # Add rows
-        for row in df.itertuples():
-            table.add_row(row)
-
-    return table.get_string(title=title)
 
 
 def main():
