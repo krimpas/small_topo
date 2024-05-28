6fba0b93ab44a33c77e365a4bb5fda41a87147bb
krimpas <krimpas@yahoo.gr>
2024-05-28 17:25:14 +0300

    Class revision 1.G13

--- code/batfish/customutils.py
+++ code/batfish/customutils.py
@@ -9,7 +9,7 @@ def process_stats(result):
         # print(f"TYPE OF RES -> {type(res)}")
         # print(f"TYPE OF RES -> {type(res.result.statistics)}")
 
-        res.result.statistics.insert(0, "Device", [f"{h}"], True)
+        res.result[h].statistics.insert(0, "Device", [f"{h}"], True)
 
     tmp_list_df = []
     for h, res in result.items():
