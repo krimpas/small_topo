5c3058095de893c5fb273d3831eac2caa610fc36
krimpas <krimpas@yahoo.gr>
2024-06-01 22:20:00 +0300

    Dataframe revision vAC02-102D

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -66,6 +66,7 @@ def main():
     # echo_nornir_result(error_result, title="L3 Interfaces Conf")
 
     # echo_nornir_result(error_result, akey="statistics", title="Stats")
+    print_title("Aggregated Statistics for all Hosts")
     tmp_df = process_stats(error_result)
 
     # tmp_pt = dataframe_to_prettytable(tmp_df, title="Summary Statistics")
