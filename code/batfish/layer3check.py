f3410b8df8f840006c268bb2222aad7ab545cb17
krimpas <krimpas@yahoo.gr>
2024-06-01 21:19:02 +0300

    Dataframe revision vAC02-102D

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -57,7 +57,7 @@ def main():
     )
     print(78 * "@")
     for h, res in error_result.items():
-        print_result(error_result[f"{h}"])
+        print_result(error_result[h])
     # echo_nornir_result(error_result, title="L3 Interfaces Conf")
 
     # echo_nornir_result(error_result, akey="statistics", title="Stats")
