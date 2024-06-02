47f63c9e9b0f003499f09b1881e7c8fbc484498c
krimpas <krimpas@yahoo.gr>
2024-06-02 11:20:53 +0300

    Dataframe revision vAC02-102D

--- code/batfish/layer3check.py
+++ code/batfish/layer3check.py
@@ -60,9 +60,9 @@ def main():
     for h, res in error_result.items():
         print_title(f"Host=[{h}]=>Erroneous L3 Interface Configuration")
         print(res.result["data"])
-        print(80 * "+")
+        print_title(f"Host=[{h}]=>Host Statistics")
         print(res.result["statistics"])
-        print_title("End of data and statistics")
+        print(80 * "+")
 
     # echo_nornir_result(error_result, title="L3 Interfaces Conf")
 
