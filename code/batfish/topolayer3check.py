60e88eeced9cfe8904f2df0511edc3351f681172
krimpas <krimpas@yahoo.gr>
2024-06-04 14:52:29 +0300

    Makefile and .drone.yml modification

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -113,7 +113,7 @@ class TopoSection(NodeSession):
                 axis=1,
             )
         ]
-        self.sot_not_in_topo = self._build_erroneous_topo2(
+        self.sot_not_in_topo = self._build_erroneous_topo(
             left_df=self.layer3_topo, right_df=self.layer3_sot
         )
 
