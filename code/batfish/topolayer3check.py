2ada4bcc9f42303f6347687dd951ff5a114a656f
krimpas <krimpas@yahoo.gr>
2024-06-04 20:01:57 +0300

    Layer3 Topo

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -191,7 +191,7 @@ class TopoSection(NodeSession):
 
         anti_join = (
             outer[(outer["_merge"] == "left_only")]
-            .drop(columns=["_merge"])
+            .drop(columns=["_merge"], axis=1)
             .reset_index(drop=True)
         )
 
