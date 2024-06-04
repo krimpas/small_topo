2d979ab00c4b8847743387be0ed6e70190e7d1db
krimpas <krimpas@yahoo.gr>
2024-06-04 13:30:38 +0300

    Makefile and .drone.yml modification

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -105,8 +105,14 @@ class TopoSection(NodeSession):
             left_df=self.actual, right_df=self.layer3_topo
         )
 
-        self.layer3_sot = self._build_sot(source_of_truth=sot)
+        tmp_layer3_sot = self._build_sot(source_of_truth=sot)
 
+        self.layer3_sot = tmp_layer3_sot[
+            tmp_layer3_sot.apply(
+                lambda row: not row["Interface"].startswith("Loop"),
+                axis=1,
+            )
+        ]
         self.sot_not_in_topo = self._build_erroneous_topo2(
             left_df=self.layer3_topo, right_df=self.layer3_sot
         )
@@ -168,7 +174,7 @@ class TopoSection(NodeSession):
 
     def get_topo(self):
         """returns topo layer3 interfaces"""
-        return self.layer3_topo, self.sot_not_in_topo
+        return self.layer3_topo, self.layer3_sot
 
     def call_method_by_name(self, name, **kwargs):
         """
