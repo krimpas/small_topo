c5e6577b97e2199f02637ffc000d927a1e940c85
krimpas <krimpas@yahoo.gr>
2024-06-04 15:20:54 +0300

    Makefile and .drone.yml modification

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -148,7 +148,7 @@ class TopoSection(NodeSession):
             left_df,
             right_df,
             on="Interface",
-            how="right",
+            how="left",
             indicator=True,
         )
 
