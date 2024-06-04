3bced9f3f0574e6c192ba51817ddd45e1084b76c
krimpas <krimpas@yahoo.gr>
2024-06-05 01:01:02 +0300

    chevking errors=2

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -114,9 +114,9 @@ class TopoSection(NodeSession):
             )
         ]
 
-        # self.sot_not_in_topo = self._build_erroneous_topo(
-        #    left_df=self.layer3_sot, right_df=self.layer3_topo
-        # )
+        self.sot_not_in_topo = self._build_erroneous_topo2(
+            left_df=self.layer3_sot, right_df=self.layer3_topo
+        )
 
     def _build_sot(self, source_of_truth: List = None) -> pd.DataFrame:
         """
