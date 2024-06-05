0d6ecc499d49213f31d9ed780ef0bd88dc7edb34
krimpas <krimpas@yahoo.gr>
2024-06-05 23:32:17 +0300

    checking errors v1

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -115,7 +115,7 @@ class TopoSection(NodeSession):
             left_df=self.actual, right_df=self.layer3_topo
         )
 
-        # self.layer3_sot = self._build_sot(source_of_truth=sot)
+        self.layer3_sot = self._build_sot(source_of_truth=sot)
 
         # self.layer3_sot = tmp_layer3_sot[
         #    tmp_layer3_sot.apply(
@@ -204,7 +204,7 @@ class TopoSection(NodeSession):
 
     def get_topo(self):
         """returns topo layer3 interfaces"""
-        return self.layer3_topo, self.actual_not_in_topo
+        return self.layer3_topo, self.layer3_sot
 
     def call_method_by_name(self, name, **kwargs):
         """
