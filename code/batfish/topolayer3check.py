3d25cfc90423524bfe2fc8712667bbf0deee5c40
krimpas <krimpas@yahoo.gr>
2024-06-05 23:57:47 +0300

    checking errors v1

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -206,7 +206,7 @@ class TopoSection(NodeSession):
 
     def get_topo(self):
         """returns topo layer3 interfaces"""
-        return self.layer3_topo, self.layer3_sot
+        return self.layer3_topo, self.actual
 
     def call_method_by_name(self, name, **kwargs):
         """
