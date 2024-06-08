f2fd085f55aac1dd6ba9be06747d7290ad55c804
krimpas <krimpas@yahoo.gr>
2024-06-08 10:18:47 +0300

    refactoring code

--- code/batfish/L3config.py
+++ code/batfish/L3config.py
@@ -177,7 +177,7 @@ class NodeL3(NodeSession):
         """
         sot_list = [
             Interface(hostname=self.node, interface=sot_item)
-            for sot_item in source_of_truth[f"{self.node}"]
+            for sot_item in source_of_truth[self.node]
         ]
         return pd.DataFrame.from_dict({"Interface": sot_list})
 
@@ -185,7 +185,7 @@ class NodeL3(NodeSession):
     def left_anti_join(
         left_df: pd.DataFrame,
         right_df: pd.DataFrame,
-        properties: str = "Declared_Names",
+        properties: str = "Interface",
     ):
         """
         Used to create the Unexpected L3 Interfaces and Missing L3
