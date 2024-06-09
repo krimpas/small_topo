5333d21ff3d181ea7379fe498ffb931a811feaf6
krimpas <krimpas@yahoo.gr>
2024-06-09 16:42:14 +0300

    refactoring / code cleanuo

--- code/batfish/topolayer3check.py
+++ code/batfish/topolayer3check.py
@@ -16,12 +16,185 @@ from customutils import process_stats, dataframe_to_prettytable, echo_nornir_res
 from L3.nodel3topo import NodeL3Topo
 
 
-ifaces = {
+ifaces_list = {
     "r1": ["GigabitEthernet2", "GigabitEthernet4", "Loopback0"],
     "r2": ["GigabitEthernet2", "GigabitEthernet3", "GigabitEthernet4", "Loopback0"],
     "r3": ["GigabitEthernet2", "GigabitEthernet3", "Loopback0"],
     "r4": ["GigabitEthernet0/1", "GigabitEthernet3", "Loopback0"],
     "r5": ["GigabitEthernet0/1", "GigabitEthernet0/2", "Loopback0", "Loopback5"],
+}
+
+
+ifaces = {
+    "r1": {
+        "interfaces": [
+            {
+                "description": "towards OSPF area 0",
+                "enabled": True,
+                "ipv4": "10.0.0.1",
+                "mask": "255.255.255.240",
+                "mtu": 1500,
+                "name": "GigabitEthernet 2",
+                "ospf_config": {
+                    "area": "0.0.0.0",
+                    "cost": 9,
+                    "net_type": "broadcast",
+                    "priority": 255,
+                },
+            },
+            {
+                "description": "towards OSPF area 51",
+                "enabled": True,
+                "ipv4": "10.123.34.1",
+                "mask": "255.255.255.224",
+                "mtu": 1500,
+                "name": "GigabitEthernet 4",
+            },
+            {
+                "enabled": True,
+                "ipv4": "172.16.1.1",
+                "mask": "255.255.255.0",
+                "name": "Loopback 0",
+                "ospf_config": {"area": "0.0.0.1", "net_type": "point-to-point"},
+            },
+        ]
+    },
+    "r2": {
+        "interfaces": [
+            {
+                "description": "towards OSPF area 0",
+                "enabled": True,
+                "ipv4": "10.0.0.2",
+                "mask": "255.255.255.240",
+                "mtu": 1500,
+                "name": "GigabitEthernet 2",
+                "ospf_config": {"cost": 10, "net_type": "broadcast", "priority": 254},
+            },
+            {
+                "description": "towards r4/OSPF Area 0.0.2.4",
+                "enabled": True,
+                "ipv4": "10.0.24.2",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 3",
+                "ospf_config": {"net_type": "point-to-point"},
+            },
+            {
+                "description": "towards r5/OSPF area 0.0.2.5",
+                "enabled": True,
+                "ipv4": "10.0.25.2",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 4",
+                "ospf_config": {"net_type": "point-to-point"},
+            },
+            {
+                "enabled": True,
+                "ipv4": "172.16.2.2",
+                "mask": "255.255.255.0",
+                "name": "Loopback 0",
+                "ospf_config": {"area": "0.0.0.0", "net_type": "point-to-point"},
+            },
+        ]
+    },
+    "r3": {
+        "interfaces": [
+            {
+                "description": "towards OSPF area 0",
+                "enabled": True,
+                "ipv4": "10.0.0.3",
+                "mask": "255.255.255.240",
+                "mtu": 1500,
+                "name": "GigabitEthernet 2",
+                "ospf_config": {
+                    "area": "0.0.0.0",
+                    "net_type": "broadCAST",
+                    "priority": 0,
+                },
+            },
+            {
+                "description": "towards r5/OSPF area 0.0.3.5",
+                "enabled": True,
+                "ipv4": "10.0.35.3",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 3",
+                "ospf_config": {
+                    "area": "0.0.3.5",
+                    "key_chain": "area0.0.3.5",
+                    "net_type": "point-to-point",
+                },
+            },
+            {
+                "enabled": True,
+                "ipv4": "172.16.3.3",
+                "mask": "255.255.255.0",
+                "name": "Loopback 0",
+                "ospf_config": {"area": "0.0.0.3", "net_type": "point-to-point"},
+            },
+        ]
+    },
+    "r4": {
+        "interfaces": [
+            {
+                "description": "towards r2/OSPF Area 0.0.2.4",
+                "enabled": True,
+                "ipv4": "10.0.24.4",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 0/1",
+                "ospf_config": {"area": "0.0.2.4", "net_type": "point-to-point"},
+            },
+            {
+                "enabled": True,
+                "ipv4": "172.16.4.4",
+                "mask": "255.255.255.0",
+                "name": "Loopback 0",
+                "ospf_config": {"area": "0.0.2.4", "net_type": "point-to-point"},
+            },
+        ]
+    },
+    "r5": {
+        "interfaces": [
+            {
+                "description": "towards r2/OSPF area 0.0.2.4",
+                "enabled": True,
+                "ipv4": "10.0.25.5",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 0/1",
+                "ospf_config": {"net_type": "point-to-point"},
+            },
+            {
+                "description": "towards r3/OSPF area 0.0.3.5",
+                "enabled": True,
+                "ipv4": "10.0.35.5",
+                "mask": "255.255.255.248",
+                "mtu": 1500,
+                "name": "GigabitEthernet 0/2",
+                "ospf_config": {
+                    "key_chain": "area 0.0.3.5",
+                    "net_type": "point-to-point",
+                },
+            },
+            {
+                "description": "just loopback ",
+                "enabled": True,
+                "ipv4": "172.16.5.5",
+                "mask": "255.255.255.0",
+                "name": "Loopback 0",
+                "ospf_config": {"net_type": "point-to-point"},
+            },
+            {
+                "description": "just loopback 1",
+                "enabled": False,
+                "ipv4": "172.16.55.55",
+                "mask": "255.255.255.0",
+                "name": "Loopback 5",
+                "ospf_config": {"net_type": "point-to-point"},
+            },
+        ]
+    },
 }
 
 load_dotenv()
