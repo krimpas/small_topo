895f77d1fd6972538c5eff579719db08f837734b
krimpas <krimpas@yahoo.gr>
2024-06-09 12:39:26 +0300

    refactoring / code cleanuo

--- code/batfish/L3/__init__.py
+++ code/batfish/L3/__init__.py
@@ -5,4 +5,4 @@ from .nodel3 import NodeL3
 from .nodel3integrity import NodeL3Integrity
 from .nodel3topo import NodeL3Topo
 from .bfilters import BatFilter
-from .nodel3conf import Nodel3Conf
+from .nodel3conf import NodeL3Conf
