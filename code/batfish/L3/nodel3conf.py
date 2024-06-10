d335b6f8c33c3f4d6ec5f93c07b9024acea81822
krimpas <krimpas@yahoo.gr>
2024-06-10 18:31:25 +0300

    refactoring / code cleanup v1.8

--- code/batfish/L3/nodel3conf.py
+++ code/batfish/L3/nodel3conf.py
@@ -64,13 +64,13 @@ class NodeL3Conf(NodeL3):
             Interface(hostname=self.node, interface=iface["name"].replace(" ", ""))
             for iface in new_sot[self.node]["interfaces"]
         ]
-        tmp_df = pd.DataFrame.from_dict({"Interface": tmp_list})
+        # tmp_df = pd.DataFrame.from_dict({"Interface": tmp_list})
         interface_info = pd.DataFrame.from_records(new_sot[self.node]["interfaces"])
-        result_df = pd.concat([tmp_df, interface_info], axis=1)
+        # result_df = pd.concat([tmp_df, interface_info], axis=1)
 
         # result_df.fillna("-", inplace=True)
-        return result_df
+        return interface_info
 
     def send_results(self) -> pd.DataFrame:
         """returns actual"""
-        return self.actual, self.actual
+        return self.actual, self.sot_info
