532aecfc271bd3d7f823d265f9ba1deae80cdeba
krimpas <krimpas@yahoo.gr>
2024-06-10 12:59:34 +0300

    refactoring / code cleanup v1.1

--- code/batfish/L3/nodel3conf.py
+++ code/batfish/L3/nodel3conf.py
@@ -76,7 +76,7 @@ class NodeL3Conf(NodeL3):
         result_df = pd.concat([tmp_df, interface_info], axis=1)
 
         result_df.fillna("-", inplace=True)
-        return result_df
+        return tmp_df
 
     def send_results(self) -> pd.DataFrame:
         """returns actual"""
