536e52558e99d975a9a8f36d016a80bf604f2b8c
krimpas <krimpas@yahoo.gr>
2024-06-11 19:48:29 +0300

    refactoring / code cleanup v1.1313

--- code/batfish/L3/nodel3conf.py
+++ code/batfish/L3/nodel3conf.py
@@ -96,7 +96,7 @@ class NodeL3Conf(NodeL3):
 
         self.sot_info["Primary_Network"] = self.sot_info.apply(self.get_IPv4net, axis=1)
 
-        self.sot_info["MTU"] = self.sot_info.apply(self.get_mtu, axis=1)
+        self.sot_info["MTU1"] = self.sot_info.apply(self.get_mtu, axis=1)
 
     @staticmethod
     def get_IPv4(row):
