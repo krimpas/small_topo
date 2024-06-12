08f1b70b9edf7b24aad02c10aa06cac756002393
krimpas <krimpas@yahoo.gr>
2024-06-12 10:13:26 +0300

    refactoring / code cleanup v1.1313

--- code/batfish/L3/nodel3conf.py
+++ code/batfish/L3/nodel3conf.py
@@ -146,12 +146,12 @@ class NodeL3Conf(NodeL3):
         outer = pd.merge(
             left_df[properties], right_df[properties], how="outer", indicator=True
         )
-        anti_join = (
-            outer[outer["_merge"] == "left_only"]
-            .drop(columns=["_merge"])
-            .reset_index(drop=True)
-        )
-        return anti_join
+        # anti_join = (
+        #    outer[outer["_merge"] == "left_only"]
+        #    .drop(columns=["_merge"])
+        #    .reset_index(drop=True)
+        # )
+        return outer
 
     def compute_ipv4_mismatch(self) -> pd.DataFrame:
         """r seis"""
