98d1564132534f9a9340c2873c9d1e69bfc61628
krimpas <krimpas@yahoo.gr>
2024-06-19 16:34:06 +0300

    refactoring / code cleanup v13

--- code/batfish/L3/nodel3conf.py
+++ code/batfish/L3/nodel3conf.py
@@ -145,7 +145,10 @@ class NodeL3Conf(NodeL3):
     @staticmethod
     def get_mtu(row):
         """Returns if the interface is enabled"""
-        return int(row["mtu"])
+        if not row["mtu"]:
+            return 1500
+        else:
+            return int(row["mtu"])
 
     def compute_mismatch(self, columns: List[str], check_column: str) -> pd.DataFrame:
         """
